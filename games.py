97596143b6962a3080cbf99c390ca7cf1984e782
Sir Beaudelaire <johnathangrx@gmail.com>
2021-07-20 00:10:38 -0600

    Deck of cards and Fisher-Yates

---
+++ games.py
@@ -0,0 +1,14 @@
+import random
+
+class Card:
+    # def __init__(self):
+        # self.value = value
+        # self.color = color
+    pass
+
+class Deck:
+    pass
+
+class Player:
+    pass
+    
